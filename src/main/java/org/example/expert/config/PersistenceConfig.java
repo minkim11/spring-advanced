f7ba5ad56f5f32d1f55dfd6b5c043dec7edc67f3
minkim11 <alsgur4772@gmail.com>
2026-05-07 20:18:18 +0900

    레벨 1 : ArgumentResolver
    PersistenceConfig 수정
    - AuthUserArgumentResolver 작동할 수 있도록 추가

--- src/main/java/org/example/expert/config/PersistenceConfig.java
+++ src/main/java/org/example/expert/config/PersistenceConfig.java
@@ -2,8 +2,17 @@ package org.example.expert.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @EnableJpaAuditing
-public class PersistenceConfig {
+public class PersistenceConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new AuthUserArgumentResolver());
+    }
 }
